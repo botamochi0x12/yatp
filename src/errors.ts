3191984318ecbc126565fa0b0ebb3d4b2c94de6a
botamochi0x12 <botamochi0x12@gmail.com>
2023-03-30 01:38:41 +0900

    style: Format error classes

--- src/errors.ts
+++ src/errors.ts
@@ -1,15 +1,15 @@
-export class InvalidSyntaxError extends Error {}
+export class InvalidSyntaxError extends Error { }
 
-export class InvalidLabelError extends InvalidSyntaxError {}
+export class InvalidLabelError extends InvalidSyntaxError { }
 
-export class InvalidCharacterDeclarationError extends InvalidSyntaxError {}
+export class InvalidCharacterDeclarationError extends InvalidSyntaxError { }
 
-export class InvalidMonologueError extends InvalidSyntaxError {}
+export class InvalidMonologueError extends InvalidSyntaxError { }
 
-export class InvalidSingleLineTagError extends InvalidSyntaxError {}
+export class InvalidSingleLineTagError extends InvalidSyntaxError { }
 
-export class InvalidMultiLineTagError extends InvalidSyntaxError {}
+export class InvalidMultiLineTagError extends InvalidSyntaxError { }
 
-export class InvalidBlockCommentError extends InvalidSyntaxError {}
+export class InvalidBlockCommentError extends InvalidSyntaxError { }
 
-export class InvalidLineCommentError extends InvalidSyntaxError {}
+export class InvalidLineCommentError extends InvalidSyntaxError { }
